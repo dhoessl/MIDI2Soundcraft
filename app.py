d7cc36e62a27149c764a6dae2516e94158c3b8c5
Dominic Hößl <dominichoessl@gmail.com>
2025-03-31 11:05:06 +0200

    make sure soundcraft wifi is active before starting the app

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 #!/root/midi_controller/bin/python3
 from services.controller import Controller
+from subprocess import Popen, PIPE
+from time import sleep
 
 if __name__ == "__main__":
+    check_network = Popen(
+        ["nmcli", "-f", "GENERAL.STATE", "con", "show", "soundcraft"],
+        stdout=PIPE,
+        stderr=PIPE
+    )
+    stdout = b''
+    while 'activated' not in stdout.decode():
+        # Waiting till nmcli connection to soundcraft is up
+        stdout, stderr = check_network.communicate()
+        sleep(.5)
     controller = Controller("10.10.1.1", "10.10.1.10")
     controller.run()
