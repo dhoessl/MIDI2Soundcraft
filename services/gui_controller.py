3c6c7c7e1d40100730efc04a948919816dc61cc5
Dominic Hößl <dominic.hoessl@noris.de>
2025-07-29 20:36:10 +0200

    fix sidebutton, mutebutton and shift button

--- services/gui_controller.py
+++ services/gui_controller.py
@@ -67,6 +67,7 @@ class GuiController:
             for x in range(5):
                 self.update_fx_params("3", f"par{x + 1}")
             self.update_bpm()
+            self.set_apc_side_button("0")
         else:
             pass  # since no logger is active here
 
@@ -116,8 +117,11 @@ class GuiController:
             self.formatter.mix(value)
         )
 
-    def set_shift_button(self, state: bool) -> None:
-        self.gui.set_shift_button(state)
+    def set_apc_side_button(self, button_id: int | str) -> None:
+        self.gui.set_apc_side_button(int(button_id))
+
+    def set_shift_button(self, state: bool, controller: str) -> None:
+        self.gui.set_shift_button(state, controller)
 
     def update_fx_params(self, channel: str | int, key: str) -> None:
         try:
@@ -158,23 +162,25 @@ class GuiController:
             )
 
     def update_mix_channels(self, increment: bool, index: int) -> None:
-        data = {}
-        for channel in range(12):
-            value_mix = float(
-                self.config.get_channel_value(str(channel), "mix")
-            )
-            data[channel] = {
-                "btns": self.vars.soundcraft_to_midi(value_mix),
-                "value": self.formatter.mix(value_mix),
-            }
-        self.gui.change_apc_channels(increment, data)
-        for lower_button in range(index, 8 + index):
-            mute_values = []
-            mute_values.append(
-                int(self.config.get_channel_value(str(lower_button), "mute"))
-            )
-        for val in mute_values:
-            self.gui.set_apc_mute_button(mute_values.index(val), bool(val))
+        for channel in range(index, index + 8):
+            self.update_apc_mix_channel(index)
+        # data = {}
+        # for channel in range(index, index + 8):
+        #     value_mix = float(
+        #         self.config.get_channel_value(str(channel), "mix")
+        #     )
+        #     data[channel] = {
+        #         "btns": self.vars.soundcraft_to_midi(value_mix),
+        #         "value": self.formatter.mix(value_mix),
+        #     }
+        # self.gui.change_apc_channels(increment, data)
+        # for lower_button in range(index, 8 + index):
+        #     mute_values = []
+        #     mute_values.append(
+        #         int(self.config.get_channel_value(str(lower_button), "mute"))
+        #     )
+        # for val in mute_values:
+        #     self.gui.set_apc_mute_button(mute_values.index(val), bool(val))
 
     def update_dial_channels(self) -> None:
         data = {}
